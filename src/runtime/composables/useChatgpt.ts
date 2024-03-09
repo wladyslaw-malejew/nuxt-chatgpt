92a7f40988b7d81cd0fe5571f9d29d6a859d898e
Vlad Maleev <vlad@jaffle.io>
2024-03-09 13:57:21 +0200

    added ability to pass dynamic api key value

--- src/runtime/composables/useChatgpt.ts
+++ src/runtime/composables/useChatgpt.ts
@@ -1,45 +1,54 @@
-import { createError } from 'h3'
-import type { IChatgptClient, IMessage, IModel, IOptions } from "../types"
+import { createError } from "h3";
+import type { IChatgptClient, IMessage, IModel, IOptions } from "../types";
 
 export const useChatgpt = (): IChatgptClient => {
-
-  const chat = async (message: IMessage, model?: IModel, options?: IOptions) => {
-
+  const chat = async (
+    message: IMessage,
+    model?: IModel,
+    options?: IOptions,
+    dynamicApiKey?: string
+  ) => {
     try {
-      return await $fetch('/api/chat', {
-        method: 'POST',
+      return await $fetch("/api/chat", {
+        method: "POST",
         body: {
           message,
           model,
-          options
-        }
-      })
+          options,
+          dynamicApiKey,
+        },
+      });
     } catch (error) {
       throw createError({
         statusCode: 500,
-        message: 'Failed to forward request to server',
-      })
+        message: "Failed to forward request to server",
+      });
     }
-  }
+  };
 
-  const chatCompletion = async (messages?: [], model?: IModel, options?: IOptions) => {
-
+  const chatCompletion = async (
+    messages?: [],
+    model?: IModel,
+    options?: IOptions,
+    dynamicApiKey?: string
+  ) => {
     try {
-      return await $fetch('/api/chat-completion', {
-        method: 'POST',
+      return await $fetch("/api/chat-completion", {
+        method: "POST",
         body: {
           messages,
           model,
-          options
-        }
-      })
+          options,
+          dynamicApiKey,
+        },
+      });
     } catch (error) {
       throw createError({
         statusCode: 500,
-        message: 'Failed to forward request to server',
-      })
+        message: "Failed to forward request to server",
+      });
     }
-  }
+  };
 
-  return { chat, chatCompletion }
-}
+  return { chat, chatCompletion };
+};
--- src/runtime/server/api/chat-completion.ts
+++ src/runtime/server/api/chat-completion.ts
@@ -1,47 +1,49 @@
-import OpenAI from 'openai'
-import { createError, defineEventHandler, readBody } from "h3"
-import { defaultOptions } from "../../constants/options"
-import { MODEL_GPT_TURBO_3_5 } from '../../constants/models'
-import { modelMap } from "../../utils/model-map"
-import { useRuntimeConfig } from '#imports'
+import OpenAI from "openai";
+import { createError, defineEventHandler, readBody } from "h3";
+import { defaultOptions } from "../../constants/options";
+import { MODEL_GPT_TURBO_3_5 } from "../../constants/models";
+import { modelMap } from "../../utils/model-map";
+import { useRuntimeConfig } from "#imports";
 
 export default defineEventHandler(async (event) => {
   // destructing the data that comes from the request
-  const { messages, model, options } = await readBody(event)
+  const { messages, model, options, dynamicApiKey } = await readBody(event);
+
+  const apiKey = dynamicApiKey || useRuntimeConfig().chatgpt.apiKey;
 
   // throw an error if the apiKey is not set
-  if (!useRuntimeConfig().chatgpt.apiKey) {
+  if (!apiKey) {
     throw createError({
       statusCode: 403,
-      message: 'Missing OpenAI API Key',
-    })
+      message: "Missing OpenAI API Key",
+    });
   }
 
   // set-up configuration object and apiKEY
   const openai = new OpenAI({
-    apiKey: useRuntimeConfig().chatgpt.apiKey
+    apiKey,
   });
 
   /**
    * Create request options object
    * @description if the model is not defined by the user it will be used the default one - gpt-3.5-turbo
-  */
+   */
   const requestOptions = {
     messages,
     model: !model ? modelMap[MODEL_GPT_TURBO_3_5] : modelMap[model],
-    ...(options || defaultOptions)
-  }
+    ...(options || defaultOptions),
+  };
 
   /**
    * @return response
-  */
+   */
   try {
-    const chatCompletion = await openai.chat.completions.create(requestOptions)
-    return chatCompletion.choices
+    const chatCompletion = await openai.chat.completions.create(requestOptions);
+    return chatCompletion.choices;
   } catch (error) {
     throw createError({
       statusCode: 500,
-      message: 'Failed to forward request to OpenAI API',
-    })
+      message: "Failed to forward request to OpenAI API",
+    });
   }
-})
+});
